5f88ca3c45e9a38778c8a1c0ccfe752241c4ab8c
alemdar1 <alemdarsu@gmail.com>
2021-06-30 20:25:21 -0400

    US105 want commit

--- src/main/java/US105.java
+++ src/main/java/US105.java
@@ -2,6 +2,7 @@ public class US105 {
     public static void main(String[] args) {
         System.out.println("selamm");
         System.out.println("huhu");
+        System.out.println("want");
     }
 
 }
